fabc7b7745384720709c841d3b5ad3ece6138422
Yang Di <young@deelol.com>
2022-04-13 23:19:11 +0800

    添加antdv

--- src/views/test/Test.vue
+++ src/views/test/Test.vue
@@ -1,11 +1,11 @@
 <template>
-<div>
-  {{ name }} 12321213
-</div>
+  <Tag>{{ name }}</Tag>
+  <Input />
 </template>
 
 <script setup>
 import {ref} from 'vue'
+import { Tag, Input } from 'ant-design-vue'
 
 const name = ref("yangdi")
 </script>
